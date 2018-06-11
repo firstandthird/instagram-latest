d2120bb602bd812a35edbe7bc88c6fe92d3f0cb5
orthagonal <orthagonal1@gmail.com>
2018-06-11 12:33:29 -0500

    first go

---
+++ index.js
@@ -0,0 +1,17 @@
+const extractIds = require('./lib/extractIdsFromHtml');
+const fetchMeta = require('./lib/fetchMetaWithId');
+const https = require('https');
+const fetch = require('node-fetch');
+
+// fetch up to 10 posts for an instagram user
+module.exports = async(instagramUser, limit=10) => {
+  const response = await fetch(`https://www.instagram.com/${instagramUser}/`);
+  const page = await response.text();
+  const idList = await extractIds(page, limit);
+  const results = [];
+  await Promise.all(idList.map(id => new Promise(async(resolve, reject) => {
+    results.push(await fetchMeta(id));
+    resolve();
+  })));
+  return results;
+};
